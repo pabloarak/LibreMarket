e8faaeee89bcf83126afe4396a844eebe7f1335b
Pablo Arak <pablo.arak.vm@gmail.com>
2023-10-24 18:29:30 -0300

    Initial files

---
+++ src/main/java/com/libre/market/LibreMarketApplication.java
@@ -0,0 +1,13 @@
+package com.libre.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/libre/market/LibreMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libre.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
