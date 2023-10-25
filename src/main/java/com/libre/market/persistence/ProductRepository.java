e02233c492400863a6d85a29f0ea86f27f655934
Pablo Arak <pablo.arak.vm@gmail.com>
2023-10-25 19:05:49 -0300

    First query methods

--- src/main/java/com/libre/market/persistence/ProductRepository.java
+++ src/main/java/com/libre/market/persistence/ProductRepository.java
@@ -4,11 +4,20 @@ import com.libre.market.persistence.crud.ProductCrudRepository;
 import com.libre.market.persistence.entity.Product;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
     public List<Product> getAll() {
         return (List<Product>) productCrudRepository.findAll();
+    }
+
+    public List<Product> getByCategory(int categoryId) {
+        return productCrudRepository.findByCategoryIdOrderByNameAsc(categoryId);
+    }
+
+    public Optional<List<Product>> getScarces(int quantity) {
+        return productCrudRepository.findByQuantityStockLessThanAndState(quantity);
     }
 }
