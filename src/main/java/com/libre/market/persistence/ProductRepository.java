3e6ed4d299a73ac509bada090ab1fdfcd06941f5
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-15 23:29:11 -0300

    Added initial methods for product manipulation

--- src/main/java/com/libre/market/persistence/ProductRepository.java
+++ src/main/java/com/libre/market/persistence/ProductRepository.java
@@ -2,10 +2,12 @@ package com.libre.market.persistence;
 
 import com.libre.market.persistence.crud.ProductCrudRepository;
 import com.libre.market.persistence.entity.Product;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductRepository {
     private ProductCrudRepository productCrudRepository;
 
@@ -19,5 +21,17 @@ public class ProductRepository {
 
     public Optional<List<Product>> getScarces(int quantity) {
         return productCrudRepository.findByQuantityStockLessThanAndState(quantity);
+    }
+
+    public Optional<Product> getProduct(int productId) {
+        return productCrudRepository.findById(productId);
+    }
+
+    public Product save(Product product) {
+        return productCrudRepository.save(product);
+    }
+
+    public void delete(int productId) {
+        productCrudRepository.deleteById(productId);
     }
 }
