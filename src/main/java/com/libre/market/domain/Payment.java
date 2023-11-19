97c31257781084788571daab43fba464faca4fed
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-19 12:11:30 -0300

    Initial domain for payments

---
+++ src/main/java/com/libre/market/domain/Payment.java
@@ -0,0 +1,70 @@
+package com.libre.market.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Payment {
+    private int paymentId;
+    private String clientId;
+    private LocalDateTime date;
+    private String paymentMethod;
+    private String comment;
+    private String state;
+    private List<PaymentItem> item;
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int purchaseId) {
+        this.paymentId = purchaseId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public List<PaymentItem> getItem() {
+        return item;
+    }
+
+    public void setItem(List<PaymentItem> item) {
+        this.item = item;
+    }
+}
