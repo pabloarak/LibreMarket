5db9b83d1a2aaaea67a08a54b512fa949eb3e5d5
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-19 14:54:08 -0300

    Added swagger 3

---
+++ src/main/java/com/libre/market/web/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.libre.market.web.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI usersMicroserviceOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Libre Market API")
+                        .description("API of a Market")
+                        .version("1.0"));
+    }
+}
