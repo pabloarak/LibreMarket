71aa6e8aef1820b067d72c2f315859926c457a0b
Pablo Arak <pablo.arak.vm@gmail.com>
2023-11-19 10:53:43 -0300

    Added first routes

--- src/main/java/com/libre/market/web/controller/ProductController.java
+++ src/main/java/com/libre/market/web/controller/ProductController.java
@@ -3,8 +3,7 @@ package com.libre.market.web.controller;
 import com.libre.market.domain.Product;
 import com.libre.market.domain.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,24 +13,29 @@ import java.util.Optional;
 public class ProductController {
     @Autowired
     private ProductService productService;
-
+    @GetMapping()
     public List<Product> getAll() {
         return productService.getAll();
     }
 
-    public Optional<Product> getProduct(int productId) {
+    @GetMapping("/{id}")
+    public Optional<Product> getProduct(@PathVariable("id") int productId) {
         return productService.getProduct(productId);
     }
 
-    public Optional<List<Product>> getByCategory(int categoryId) {
+    @GetMapping("/category/{id}")
+    public Optional<List<Product>> getByCategory(@PathVariable("id") int categoryId) {
         return productService.getByCategory(categoryId);
     }
 
-    public Product save(Product product) {
+    @PostMapping()
+    public Product save(@RequestBody Product product) {
+        System.out.println(product);
         return productService.save(product);
     }
 
-    public boolean delete(int productId) {
+    @DeleteMapping("/{id}")
+    public boolean delete(@PathVariable("id") int productId) {
         return productService.delete(productId);
     }
 }
